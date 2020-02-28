6970decde4ad4799f34bd46f77dd53f229c60723
MoeiSadeghi <mohammad_sadeghi96@yahoo.com>
2020-02-28 09:23:40 +0100

    Initial Commit

---
+++ Face Recognizer-SwiftP10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Face Recognizer-SwiftP10
+//
+//  Created by Mohammadreza Sadeghi on 28/02/2020.
+//  Copyright © 2020 Mohammadreza Sadeghi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
